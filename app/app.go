0f2a66b3a76c62fd793ddb8c6054b810eb081eb0
suhassuhas <suhas7thfeb2000@gmail.com>
2022-04-09 20:35:51 +0530

    <ORDER> | Added Health Route | <Suhas>

---
+++ app/app.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "github.com/swiggy-2022-bootcamp/cdp-team4/Orders/docs"
+)
+
+func Start() {
+	// Gin instance
+	r := gin.New()
+
+	// Routes
+	r.GET("/", HealthCheck)
+
+	url := ginSwagger.URL("http://localhost:7000/swagger/doc.json") // The url pointing to API definition
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
+	// Start server
+	if err := r.Run(":7000"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HealthCheck(c *gin.Context) {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	c.JSON(http.StatusOK, res)
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/swiggy-2022-bootcamp/cdp-team4/Orders/app"
+)
+
+// @title Gin Swagger Example API
+// @version 2.0
+// @description Order Service.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:7000
+// @BasePath /
+// @schemes http
+func main() {
+	app.Start()
+}
