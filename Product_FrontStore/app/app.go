594886bd49d9c0f213c88ad3d8dc479a01a6b272
poojasethia <pooja16041@iiitd.ac.in>
2022-04-10 18:17:23 +0530

    <PRODUCT_FRONTSTORE>|<Dockerized healthcheck endpoint>|<pooja>

---
+++ Product_FrontStore/app/app.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/joho/godotenv"
+	"github.com/swiggy-2022-bootcamp/cdp-team4/Product_FrontStore/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	// health check route
+	HealthCheckRouter(router)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	return router
+}
+
+func configureSwaggerDoc() {
+	docs.SwaggerInfo.Title = "Swagger Product Front Store API"
+}
+
+func Start() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	PORT := os.Getenv("PORT")
+
+	configureSwaggerDoc()
+	router := setupRouter()
+
+	router.Run(":" + PORT)
+}
