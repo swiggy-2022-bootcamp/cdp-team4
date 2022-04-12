18374264738e255523d41142cb808a8d25ee1183
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-04-12 21:10:01 +0530

    <reward> | Health Check route and Dockerised Service| <Pradyuman>

---
+++ reward/app/healthHandler.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Health Check
+// @Summary      Health of Reward service
+// @Description  Endpoint to check the health of Reward Microserice
+// @Tags         Health
+// @Produce      json
+// @Success      200  {object}  map[string]interface{}
+// @Failure      400  {number} 	http.StatusBadRequest
+// @Router       /    [get]
+func HealthCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Service is running"})
+	}
+}
