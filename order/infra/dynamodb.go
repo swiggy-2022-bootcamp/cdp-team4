d1cc4a84784fa082cdb357e58a524e4a4fc77530
suhassuhas <suhas7thfeb2000@gmail.com>
2022-04-15 21:40:01 +0530

    <ORDER> | Modified Dynamodb config | <Suhas>

--- order/infra/dynamodb.go
+++ order/infra/dynamodb.go
@@ -21,14 +21,9 @@ type OrderDynamoRepository struct {
 }
 
 func connect() *dynamodb.DynamoDB {
-	sess, err := session.NewSession(&aws.Config{
-		Region:   aws.String("us-east-1"),
-		Endpoint: aws.String("http://localhost:8042"),
-	})
-
-	if err != nil {
-		panic(err.Error())
-	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
 	// create dynamo client
 	svc := dynamodb.New(sess)
