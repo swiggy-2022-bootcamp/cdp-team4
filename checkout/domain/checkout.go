47f4d94753cd08a8247b1c6620d4bcdd02da93e6
nitesh585 <niteshyadav585@gmail.com>
2022-04-21 15:07:25 +0530

    <Checkout> | add shipping gRPC client | <nitesh>

--- checkout/domain/checkout.go
+++ checkout/domain/checkout.go
@@ -2,8 +2,10 @@ package domain
 
 import (
 	context "context"
+	"fmt"
 
 	"github.com/swiggy-2022-bootcamp/cdp-team4/checkout/protos"
+	"google.golang.org/grpc"
 )
 
 type Checkout struct {
@@ -21,7 +23,20 @@ func (ch *Checkout) OrderOverview(
 	// Get the User Cart details by ID 		[grpc call to Cart service]
 	// Get the Reward points details by ID 	[grpc call to Reward service]
 	// Get the Shipping details by ID 		[grpc call to Shipping service]
+	conn, err := grpc.Dial("localhost:8001", grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect with grpc server")
+	}
+	defer conn.Close()
 
+	client := protos.NewShippingClient(conn)
+
+	response, err := client.GetShippingCost(ctx, &protos.ShippingCostRequest{City: "Chennai"})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Print(response, err)
 	return &protos.OverviewResponse{
 		UserID:               rq.GetUserID(),
 		TotalPrice:           15,
