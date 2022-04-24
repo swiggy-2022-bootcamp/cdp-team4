7dd7c69ec5b0a4a9eefe002ebf7d6d81b748d7f7
Swastik Sahoo 8082 <swastiksahoo22@gmail.com>
2022-04-24 15:00:24 +0530

    <Gateway> | Handle get user | <swastik>

--- gateway/app/userHandler.go
+++ gateway/app/userHandler.go
@@ -3,6 +3,12 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	// "log"
+	// "fmt"
+	"encoding/json"
+	"context"
+	"time"
 )
 
 type userHandler struct {
@@ -22,7 +28,50 @@ func (uh userHandler) GetAllUsers(c *gin.Context) {
 }
 
 func (uh userHandler) GetUser(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"userId": c.Param("userId")})
+
+	userServiceURI := os.Getenv("USER_SERVICE_URI") + "/user/" + c.Param("userId")
+
+	req, err := http.NewRequest("GET", userServiceURI, nil)
+	if err != nil {
+		responseDto := ResponseDTO{
+			Status:  http.StatusBadRequest,
+			Message: err.Error(),
+		}
+		c.JSON(responseDto.Status, responseDto)
+		c.Abort()
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(req.Context(), 1*time.Minute)
+	defer cancel()
+
+	req = req.WithContext(ctx)
+	client := http.DefaultClient
+	res, err1 := client.Do(req)
+	if err1 != nil {
+		responseDto := ResponseDTO{
+			Status:  http.StatusBadRequest,
+			Message: err1.Error(),
+		}
+		c.JSON(responseDto.Status, responseDto)
+		c.Abort()
+		return
+	}
+
+	var userResDTO ResponseDTO
+	err = json.NewDecoder(res.Body).Decode(&userResDTO)
+
+	if err != nil {
+		responseDto := ResponseDTO{
+			Status:  http.StatusBadRequest,
+			Message: err.Error(),
+		}
+		c.JSON(responseDto.Status, responseDto)
+		c.Abort()
+		return
+	}
+
+	c.JSON(userResDTO.Status, userResDTO)
 	c.Abort()
 	return
 }
