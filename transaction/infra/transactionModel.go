445d707c582e7627d2b6c70b5e074c9d5eaaf260
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-04-25 18:12:13 +0530

    <transaction>|Setup Infra Layer|<Pradyuman>

--- transaction/infra/transactionModel.go
+++ transaction/infra/transactionModel.go
@@ -1,1 +1,13 @@
 package infra
+
+import (
+	"time"
+)
+
+type TransactionModel struct {
+	Id                string    `json:"id"`
+	UserID            string    `json:"user_id"`
+	TransactionPoints int       `json:"transaction_points"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
