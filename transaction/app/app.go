2d90671c3d7f77b7b7f9109ec4619ef3aab9d830
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-04-26 08:50:14 +0530

    <transaction>| Added GRPC server and handler | <pradyuman>

--- transaction/app/app.go
+++ transaction/app/app.go
@@ -1,23 +1,34 @@
 package app
 
 import (
-	"log"
+	"fmt"
+	"net"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	pb "github.com/swiggy-2022-bootcamp/cdp-team4/transaction/app/protos"
 	"github.com/swiggy-2022-bootcamp/cdp-team4/transaction/docs"
+	"github.com/swiggy-2022-bootcamp/cdp-team4/transaction/domain"
+	"github.com/swiggy-2022-bootcamp/cdp-team4/transaction/infra"
+	"github.com/swiggy-2022-bootcamp/cdp-team4/transaction/infra/logger"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
+var log logrus.Logger = *logger.GetLogger()
+var transactionHandler TransactionHandler
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 	// health check route
 	HealthCheckRouter(router)
-
+	transactionRouter(router, transactionHandler)
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return router
 }
@@ -26,7 +37,34 @@ func configureSwaggerDoc() {
 	docs.SwaggerInfo.Title = "Swagger Transaction API"
 }
 
+func startGrpcTransactionServer(th TransactionHandler) {
+	grpcServer := grpc.NewServer()
+	transactionServer := NewTransactionGrpcServer()
+	pb.RegisterTransactionServer(grpcServer, transactionServer)
+	fmt.Println("register server")
+	reflection.Register(grpcServer)
+	err := godotenv.Load(".env")
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	GRPC_REWARD_PORT := os.Getenv("GRPC_PORT")
+	fmt.Println(GRPC_REWARD_PORT)
+	l, err := net.Listen("tcp", ":"+GRPC_REWARD_PORT)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	fmt.Print("grpc server started")
+	grpcServer.Serve(l)
+}
+
 func Start() {
+	dynamoRepository := infra.NewDynamoRepository()
+	transactionHandler = TransactionHandler{TransactionService: domain.NewTransactionService(dynamoRepository)}
+
+	go startGrpcTransactionServer(transactionHandler)
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal(err)
---
+++ transaction/app/transactionGrpcHandler_test.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	pb "github.com/swiggy-2022-bootcamp/cdp-team4/transaction/app/protos"
+	"google.golang.org/grpc"
+)
+
+func TestGrpcTransactionClient(t *testing.T) {
+	// Set up connection with the grpc server
+
+	conn, err := grpc.Dial("localhost:7007", grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("Error while making connection, %v\n", err)
+	}
+
+	// Create a client instance
+	client := pb.NewTransactionClient(conn)
+
+	// Lets invoke the remote function from client on the server
+	resp, err := client.GetTransactionPoints(
+		context.Background(),
+		&pb.GetTransactionPointsRequest{
+			UserId: "b",
+		},
+	)
+	fmt.Println(resp)
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(165), resp.TransactionPoints)
+}
