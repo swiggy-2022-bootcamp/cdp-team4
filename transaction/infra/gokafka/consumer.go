ef1fea9960b17996b3ae818017fb51cea3d404bd
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-04-30 12:29:59 +0530

    <Cart, Transaction>|Update Kafka port to 9091|<Pradyuman>

--- transaction/infra/gokafka/consumer.go
+++ transaction/infra/gokafka/consumer.go
@@ -10,7 +10,7 @@ import (
 )
 
 var brokers = []string{
-	"localhost:9090",
+	"localhost:9091",
 }
 
 func getKafkaReader(ctx context.Context, topic, groupID string, brokers []string) *kafka.Reader {
